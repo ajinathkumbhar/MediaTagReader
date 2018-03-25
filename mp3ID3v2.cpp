bf65915f81aeaf96a347c9be25173fa109756f92
Ajinath Kumbhar <ajinathkumbhar@gmail.com>
2018-03-25 12:26:11 +0530

    basic file read

--- mp3ID3v2.cpp
+++ mp3ID3v2.cpp
@@ -1,7 +1,31 @@
-#include<stdio.h>
+/*
+ID3v2/file identifier   "ID3"
+ID3v2 version           $03 00
+ID3v2 flags             %abc00000
+ID3v2 size              4 * %0xxxxxxx
 
-main()
-{
+*/
 
+#include <stdio.h>
+
+int main () {
+   FILE *fp;
+   int c,i,byteCount;
+   i=1;
+   byteCount=3;
+  
+   fp = fopen("02.mp3","r");
+   while(i <= byteCount) {
+      c = fgetc(fp);
+      if( feof(fp) ) { 
+         break ;
+      }
+      printf("%c", c);
+      i++;
+   }
+   printf("\n");
+   fclose(fp);
+   
+   return(0);
 }
 
