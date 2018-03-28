65914dacdce52e1471ec7811f2e3b37023838ed1
shinusmp <37688382+shinusmp@users.noreply.github.com>
2018-03-28 18:58:56 +0000

    adding test file

---
+++ firsttry.c
@@ -0,0 +1,11 @@
+/*********************
+Hi
+Now to MediaTagReader dir
+create new file 
+add some junk content
+then 
+git status
+git add filename
+git commit -m "Adding test file"
+git push origin master
+********************/
